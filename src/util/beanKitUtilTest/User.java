ae78c691ab0b325a3e5371acc855a90092e95268
EddieYao <2683361531@qq.com>
2019-04-30 22:08:06 +0800

    增加BeanKit工具类以及测试BeanKit工具测试类

---
+++ src/util/beanKitUtilTest/User.java
@@ -0,0 +1,28 @@
+package util.beanKitUtilTest;
+
+public class User {
+    public User(String id, String test) {
+        this.id = id;
+        this.test = test;
+    }
+    //
+    private String id;
+    private String test;
+    private String set;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTest() {
+        return test;
+    }
+
+    public void setTest(String test) {
+        this.test = test;
+    }
+}
